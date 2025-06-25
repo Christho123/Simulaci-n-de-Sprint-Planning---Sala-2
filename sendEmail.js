8911a281ff05c37f08eae781ae62c7ed78f94ac5
Kenntomi <marci.kenntomi@gmail.com>
2025-06-25 14:02:52 -0500

    Update sendEmail.js

--- sendEmail.js
+++ sendEmail.js
@@ -5,8 +5,8 @@ const sendEmail = async (to, subject, text) => {
   const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASS,
+      user: 'clinicasenati@gmail.com',
+      pass: 'aaos kdto wfzb wfct',
     },
   });
 
